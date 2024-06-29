257903a09a3c82f84be5396849e2e20d03c0fd64
CG-AA <syankuan@gmail.com>
2024-06-29 23:34:05 +0800

    fix a lot of bugs and it worked!

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,14 @@
-FROM ubuntu:latses AS build
+FROM ubuntu:latest AS build
 
 RUN apt-get update && apt-get install -y \
     git \
     cmake \
     libssl-dev \
     libcurl4-openssl-dev \
-    g++
+    g++ \
+    nlohmann-json3-dev
 
-WORKDIR /building
+WORKDIR /app/deps
 
 RUN git clone https://github.com/yourWaifu/sleepy-discord.git && \
     cd sleepy-discord && \
@@ -26,8 +27,4 @@ RUN mkdir build && \
     cmake .. && \
     make
 
-FROM ubuntu:latest AS runtime
-
-WORKDIR /app
-
-COPY --from=build /app/build/bot .
+CMD ["./build/Lamb"]
--- main.cpp
+++ main.cpp
@@ -4,9 +4,30 @@
  * 
  * using sleepy-discord library.
  */
+#include "sleepy_discord/sleepy_discord.h"
+#include <nlohmann/json.hpp>
+#include <fstream>
 
-#include <sleepy_discord.h>
+auto getSettings() {
+    std::ifstream file("settings.json");
+    nlohmann::json j;
+    file >> j;
+    file.close();
+    return j;
+}
 
+class MyClientClass : public SleepyDiscord::DiscordClient {
+public:
+	using SleepyDiscord::DiscordClient::DiscordClient;
+	void onMessage(SleepyDiscord::Message message) override {
+		if (message.startsWith("Hi Lamb!"))
+			sendMessage(message.channelID, "Hello " + message.author.username + "!");
+	}
+};
 
 int main() {
+    auto settings = getSettings();
+	MyClientClass client(settings["token"], SleepyDiscord::USER_CONTROLED_THREADS);
+	client.setIntents(SleepyDiscord::Intent::SERVER_MESSAGES);
+	client.run();
 }
